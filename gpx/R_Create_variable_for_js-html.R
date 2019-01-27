1de6ae08a1722b1deb6de967e1b3122e28110e10
DGrv <15691063+DGrv@users.noreply.github.com>
2019-01-27 23:31:20 +0100

    Update leaflet with gpx list

---
+++ gpx/R_Create_variable_for_js-html.R
@@ -0,0 +1,32 @@
+library(data.table)
+
+wd <- "D:/DG-Papers/GitHub/Website/dorian.gravier.github.io/gpx"
+setwd(wd)
+
+lis <- data.table(path = list.files(wd, full = T, recursive = T))
+lis[, path2 := gsub("D:/DG-Papers/GitHub/Website/dorian.gravier.github.io/", "", path) ]
+lis[, dir := dirname(path2) ]
+lisdir <- unique(lis$dir)
+
+
+write("// create file", file = "Name_var_js-html_tocopy_in_leaflet.txt")
+for(i in seq_along(lisdir)) {
+  if( basename(lisdir[i]) == "Project" ) {
+    varname <- paste0(basename(dirname(lisdir[i])), basename(lisdir[i]))
+  } else {
+    varname <- basename(lisdir[i])
+  }
+  write(paste0("var ", varname, " = ["), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
+  
+  lis2 <- lis[dir == lisdir[i]]
+  
+  for( j in 1:nrow(lis2)) {
+    if( j == nrow(lis2)) {
+      write(paste0("'", lis2[j, path2], "']\n"), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
+    } else {
+      write(paste0("'", lis2[j, path2], "',"), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
+    }
+  }
+  
+}
+
