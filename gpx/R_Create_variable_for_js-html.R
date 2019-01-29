affaff05a392fbc92abc24d769da927957024e2d
DGrv <dorian.gravier@gmail.com>
2019-01-29 18:55:56 +0100

    update md

--- gpx/R_Create_variable_for_js-html.R
+++ gpx/R_Create_variable_for_js-html.R
@@ -14,21 +14,23 @@ lisdir <- unique(lis$dir)
 
 write("// create file", file = "Name_var_js-html_tocopy_in_leaflet.txt")
 for(i in seq_along(lisdir)) {
-  if( basename(lisdir[i]) == "Project" ) {
-    varname <- paste0(basename(dirname(lisdir[i])), basename(lisdir[i]))
-  } else {
-    varname <- basename(lisdir[i])
-  }
-
-  lis2 <- lis[dir == lisdir[i]]
-
-  for( j in 1:nrow(lis2)) {
-    if( j == 1) {
-      write(paste0("var ", varname, " = ['", lis2[j, path2], "',\n"), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
-    } else if( j == nrow(lis2)) {
-      write(paste0("'", lis2[j, path2], "']\n"), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
+  if( lisdir[i] != "gpx" ) {
+    if( basename(lisdir[i]) == "Project" ) {
+      varname <- paste0(basename(dirname(lisdir[i])), basename(lisdir[i]))
     } else {
-      write(paste0("'", lis2[j, path2], "',"), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
+      varname <- basename(lisdir[i])
+    }
+
+    lis2 <- lis[dir == lisdir[i]]
+
+    for( j in 1:nrow(lis2)) {
+      if( j == 1) {
+        write(paste0("var ", varname, " = ['", lis2[j, path2], "',"), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
+      } else if( j == nrow(lis2)) {
+        write(paste0("'", lis2[j, path2], "']\n"), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
+      } else {
+        write(paste0("'", lis2[j, path2], "',"), file = "Name_var_js-html_tocopy_in_leaflet.txt", append = T)
+      }
     }
   }
 }
