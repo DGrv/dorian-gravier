45f0cb5b0337f57e7942ab68befa3066ecfc4252
gravier <dorian.gravier@gmail.com>
2019-05-20 16:50:18 +0200

    magick

---
+++ files/Magick/MAGICK_Merge_Vertical_jpg-png_v02.bat
@@ -0,0 +1,41 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+set /p dir=Where are you file ? 
+set /p which="Do you want to merge jpg (type 1) or png (type 2) ? : "
+
+if %which%==1 set format="jpg"
+if %which%==2 set format="png"
+
+set /p per=Percent to resize (numeric) (make a copy it will overwrite your files)?: 
+
+
+for %%a in (%dir%) do (
+	set drive=%%~da
+)  
+%drive%
+cd "%dir%"
+
+:: give info on what will be done
+dir /a-d /b "*%format%" | find /c %format% > temp
+set /p nfiles= < temp
+::set /a nfiles=nfiles
+del temp
+
+:: resize
+if NOT %per%==100 (
+	for %%p in (*.%format%) do magick convert %%p -resize %per%%% %%p
+)
+
+
+:: Merge
+magick montage *.%format% -tile 1x%nfiles% -geometry +0+0 Merge.%format%
+magick convert Merge.%format% Merge.pdf
+
+echo DEBUG --------------------------
+echo drive %drive%
+echo dir %dir% 
+echo which %which%
+echo format %format%
+echo magick montage *.%format% -tile 1x%nfiles% -geometry +0+0 Merge.%format%
+echo magick convert Merge.%format% Merge.pdf
