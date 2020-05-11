cb7d8f31b599739adbcaff5efee8792b38442a72
DGrv <15691063+DGrv@users.noreply.github.com>
2020-05-11 11:03:23 +0200

    youtube-dl update

--- files/Batch/Youtube-dl/test.bat
+++ files/Batch/Youtube-dl/test.bat
@@ -1,19 +1,9 @@
-@echo on
-SETLOCAL EnableDelayedExpansion
-
-echo.
-set /p newfolder="Do you want to dowload it in a new folder ? [y/n]  " 
-echo.
-
-echo %newfolder%
-
-if "%newfolder%"=="y" (
-	set TIMESTAMP=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%-%TIME:~0,2%%TIME:~3,2%
-	:: Create a new directory
-	mkdir !TIMESTAMP!
-	cd !TIMESTAMP!
+set /a check=%DATE:~5,1%
+set check2=%DATE:~5,1%
+echo %check%
+echo %check2%
+if "%check%"=="%check2%" (
+	echo numeric
+) else (
+	echo not numeric
 )
-
-echo %TIMESTAMP%
-
-pause
