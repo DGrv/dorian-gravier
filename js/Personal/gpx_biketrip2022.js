c3c75704c9b8ece65651f85387058563ea667796
DGrv <dorian.gravier@gmail.com>
2022-09-14 12:12:11 +0200

    gpx

--- js/Personal/gpx_biketrip2022.js
+++ js/Personal/gpx_biketrip2022.js
@@ -7,5 +7,14 @@ var Bike_trip_2022 = ['files/gpx/Bike_trip_2022/B_2022_Bike-travel_001.gpx',
 'files/gpx/Bike_trip_2022/B_2022_Bike-travel_006.gpx',
 'files/gpx/Bike_trip_2022/B_2022_Bike-travel_007.gpx',
 'files/gpx/Bike_trip_2022/B_2022_Bike-travel_008.gpx',
-'files/gpx/Bike_trip_2022/B_2022_Bike-travel_009.gpx']
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_009.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_010.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_011.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_012.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_013.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_014.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_015.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_016.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_017.gpx',
+'files/gpx/Bike_trip_2022/B_2022_Bike-travel_018.gpx']
 
