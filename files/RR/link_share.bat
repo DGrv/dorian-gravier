aae0854da3042ee690e00089203d25429fe2b7a8
DGrv <dorian.gravier@gmail.com>
2025-02-24 15:58:55 +0100

    up

--- files/RR/link_share.bat
+++ files/RR/link_share.bat
@@ -1,4 +1,4 @@
-start "" https://events.raceresult.com/presenters/?event=323059&n=Helfer&k=HgAGIkPxZcpz
-start "" https://events.raceresult.com/presenters/?event=323059&n=SPEAKER&k=HJi7nF4eQszY
-start "" https://events.raceresult.com/registrations/?event=323059&n=Single%20Registration&k=n1Oyb7wEznOi
-start "" https://events.raceresult.com/kiosks/?event=323059&n=Chip%20Assignment&k=OqIk5HMPayId
+start "" "https://events.raceresult.com/presenters/?event=323059&n=Helfer&k=HgAGIkPxZcpz"
+start "" "https://events.raceresult.com/presenters/?event=323059&n=SPEAKER&k=HJi7nF4eQszY"
+start "" "https://events.raceresult.com/registrations/?event=323059&n=Single%%20Registration&k=n1Oyb7wEznOi"
+start "" "https://events.raceresult.com/kiosks/?event=323059&n=Chip%%20Assignment&k=OqIk5HMPayId"
