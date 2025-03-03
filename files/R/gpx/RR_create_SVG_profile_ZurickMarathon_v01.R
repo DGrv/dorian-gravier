f989a6adebc58e2ff5b2406bfd73be14a193b9cd
DGrv <dorian.gravier@gmail.com>
2025-03-03 18:29:24 +0100

    up

--- files/R/gpx/RR_create_SVG_profile_ZurickMarathon_v01.R
+++ files/R/gpx/RR_create_SVG_profile_ZurickMarathon_v01.R
@@ -24,20 +24,17 @@ setwd(wd)
 
 # comment if you do not know
 
-# ylim1 <- 350
-# ylim2 <- 550
-# dist.wanted <- 42.2
-# nylabel <- 100
-# breaksx <- 5
-
-input.gpx <- rP("file:///C:/Users/doria/Downloads/gdrive/RR/2024/20240810_SwissAlps100/Course/Swiss%20Alps%20100%20-%20160KM.gpx")
-output.name <- gsub(".gpx", ".svg", basename(input.gpx))
+ylim1 <- 350
+ylim2 <- 550
+dist.wanted <- 42.2
+nylabel <- 100
+breaksx <- 5
 
 
 # GPX --------------------------------------------------------------------
 
-data <- read.gpx(input.gpx, type="trk")
-# data <- read.gpx(rP("file:///C:/Users/doria/Downloads/gdrive/RR/2025/20250413_ZuerichMarathon/Course/zhm2025marathon.gpx"), type="trk")
+# data <- read.gpx(rP("file:///C:/Users/doria/Downloads/gdrive/RR/2024/20240810_SwissAlps100/Course/Swiss%20Alps%20100%20-%20160KM.gpx"), type="trk")
+data <- read.gpx(rP("file:///C:/Users/doria/Downloads/gdrive/RR/2025/20250413_ZuerichMarathon/Course/zhm2025marathon.gpx"), type="trk")
 
 # Calculate dist btw points
 data[, dist:=0]
@@ -64,14 +61,14 @@ if( exists("ylim1") ) {
 # TimingPoints ------------------------------------------------------------
 
 # cd <- data.table(read.table("clipboard", sep="\t", header=TRUE))
-cd <- structure(list(Split = c("Bürkliplatz - 4 km", "Bürkliplatz - 8 km",
-                               "Quaibrücke - 8.3 km", "Bellerivestrasse - 10 km", "Seestrasse - 15.8 km",
+cd <- structure(list(Split = c("BÃ¼rkliplatz - 4 km", "BÃ¼rkliplatz - 8 km",
+                               "QuaibrÃ¼cke - 8.3 km", "Bellerivestrasse - 10 km", "Seestrasse - 15.8 km",
                                "Meilen - 20 km", "Half Marathon - 21.1 km", "Meilen - 22.1 km",
-                               "Küsnacht - 30 km", "Küsnacht Goldbach - 31.2 km", "Quaibrücke - 36 km",
-                               "Mythenquai - 37.2 km", "Sihlstrasse - 40 km", "Bürkliplatz - 40.9 km",
-                               "Quaibrücke - 42 km"), dist = c(4, 8, 8.3, 10, 15.8, 20, 21.1,
-                                                               22.1, 30, 31.2, 36, 37.2, 40, 40.9, 42)), row.names = c(NA, -15L
-                                                               ), class = c("data.table", "data.frame"))
+                               "KÃ¼snacht - 30 km", "KÃ¼snacht Goldbach - 31.2 km", "QuaibrÃ¼cke - 36 km",
+                               "Mythenquai - 37.2 km", "Sihlstrasse - 40 km", "BÃ¼rkliplatz - 40.9 km",
+                               "QuaibrÃ¼cke - 42 km"), dist = c(4, 8, 8.3, 10, 15.8, 20, 21.1,
+                                                             22.1, 30, 31.2, 36, 37.2, 40, 40.9, 42)), row.names = c(NA, -15L
+                                                             ), class = c("data.table", "data.frame"))
 cd
 
 data[, Split := NULL]
@@ -94,7 +91,6 @@ if( !exists("breaksx") ) {
   breaksx <- 10
 }
 
-
 # Create the elevation profile plot
 p <- ggplot(data, aes(x = dist, y = ele)) +
   geom_polygon(fill="blue")+
@@ -108,28 +104,28 @@ p <- ggplot(data, aes(x = dist, y = ele)) +
   # geom_line(aes(y=ylim1 + (ele-ylim1)*(0.1*7), x = dist - (0.005*7)), size = 0.1)+
   # geom_line(aes(y=ylim1 + (ele-ylim1)*(0.1*8), x = dist - (0.005*8)), size = 0.1)+
   # geom_line(aes(y=ylim1 + (ele-ylim1)*(0.1*9), x = dist - (0.005*9)), size = 0.1)+
-  # geom_point(data=data[Split!=""], size=3)+
-  # geom_label_repel(data=data[Split!=""],aes(label = Split), vjust = 0, hjust= 0, nudge_y = nylabel, nudge_x=-2, direction = "y", size=3)+
+  geom_point(data=data[Split!=""], size=3)+
+  geom_label_repel(data=data[Split!=""],aes(label = Split), vjust = 0, hjust= 0, nudge_y = nylabel, nudge_x=-2, direction = "y", size=3)+
   theme_set(theme_bw())+
   scale_x_continuous(expand = c(0, 0), limits = c(0, NA), breaks = seq(0, max(data$dist), by = breaksx))+  # Remove space before 0 on x-axis
-  labs(x = "", y = "")
+  labs(x = "", y = "")+
   
-  if( exists("ylim2") ) {
-    p <- p+coord_cartesian(ylim=c(NA, ylim2))
-  }
+if( exists("ylim2") ) {
+  p <- p+coord_cartesian(ylim=c(NA, ylim2))
+}
 p
 
 # Save as SVG
-ggsave("temp.svg", plot = p, device = "svg", width = 4000, height = 1000, units = "px")
+ggsave("elevation_profile.svg", plot = p, device = "svg", width = 4000, height = 1000, units = "px")
 
 
 # Modification  ------------------------------------------------------------------
 
-t <- data.table(raw=readLines("temp.svg"))
+t <- data.table(raw=readLines("elevation_profile.svg"))
 t[, raw2:= gsub("#0000FF", "url(#linear-gradient)", raw)]
 t[, raw2:= gsub('"', "'", raw2)]
 t[, raw2:= gsub("lass='svglite' width='.*' height='.*' viewBox", "lass='svglite' viewBox", raw2)]
 t[, raw2:= gsub("<defs>", "<defs><linearGradient id='linear-gradient' x1='0%' x2='20%' y1='0%' y2='0%'><stop offset='100%' stop-color='rgba(20, 136, 202,0.8)'></stop><stop offset='100%' stop-color='rgba(175, 223, 250,0.4)'></stop></linearGradient>", raw2)]
 
-write.table(t$raw2, output.name, row.names = F, col.names = F, quote = F)
-file.remove("temp.svg")
+write.table(t$raw2, "test.svg", row.names = F, col.names = F, quote = F)
+
