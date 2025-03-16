4507de50d24042198793fd6e2bba43bd749ecad1
DGrv <dorian.gravier@gmail.com>
2025-03-17 00:13:39 +0100

    up

--- files/RR/Javascript/Leaflet-functions_rrst_v01.js
+++ files/RR/Javascript/Leaflet-functions_rrst_v01.js
@@ -1,57 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
 
-    function addMarkerToLeafletMap(idhtmlwidget, latM, lonM, flag, icon1, icon2) {
-        var widget = window.HTMLWidgets.find(idhtmlwidget);
+function addMarkerToLeafletMap(idhtmlwidget, latM, lonM, flag, icon1, icon2) {
+    var widget = window.HTMLWidgets.find(idhtmlwidget);
 
-        if (widget) {
-            var map = widget.getMap();
+    if (widget) {
+        var map = widget.getMap();
 
-            if (map) {
-                // console.log("Leaflet map is ready. Adding marker...");
+        if (map) {
+            // console.log("Leaflet map is ready. Adding marker...");
 
-                // Choose color based on status
-                // Additional information about the systems position / movement at the time of upload.
-                // U : Unknown
-                // S : Box was stationary within the last minute
-                // M : Box was moving within the last minute
-                // T : Box was moving with a speed of at least 5km/h in the last minute
-                // X : Position may not be accurate (box has moved since last GPS fix)
-                var iconEnd = flag === "X" ? icon2 : icon1;
+            // Choose color based on status
+            // Additional information about the systems position / movement at the time of upload.
+            // U : Unknown
+            // S : Box was stationary within the last minute
+            // M : Box was moving within the last minute
+            // T : Box was moving with a speed of at least 5km/h in the last minute
+            // X : Position may not be accurate (box has moved since last GPS fix)
+            var iconEnd = flag === "X" ? icon2 : icon1;
 
-                // Define custom Awesome Icon using Font Awesome
-                var awesomeIcon = L.icon({
-                    iconUrl: iconEnd,
-                    iconSize: [50, 50], // Size of the icon
-                    iconAnchor: [15, 30], // Point of the icon that will correspond to marker's location
-                    popupAnchor: [0, -30] // Popup position when opened
-                });
+            // Define custom Awesome Icon using Font Awesome
+            var awesomeIcon = L.icon({
+                iconUrl: iconEnd,
+                iconSize: [50, 50], // Size of the icon
+                iconAnchor: [15, 30], // Point of the icon that will correspond to marker's location
+                popupAnchor: [0, -30] // Popup position when opened
+            });
 
-                // Add marker dynamically
-                // var markerAB = L.marker([latM, lonM])
-                var markerAB = L.marker([latM, lonM], { icon: awesomeIcon })
-                    .bindPopup("1st")
-                    .openPopup();
+            // Add marker dynamically
+            // var markerAB = L.marker([latM, lonM])
+            var markerAB = L.marker([latM, lonM], { icon: awesomeIcon })
+                .bindPopup("1st")
+                .openPopup();
 
-                // Add marker to the LayerGroup
-                aktivBoxLayer.addLayer(markerAB);
+            // Add marker to the LayerGroup
+            aktivBoxLayer.addLayer(markerAB);
 
-                // Ensure the layer is added to the map
-                aktivBoxLayer.addTo(map);
+            // Ensure the layer is added to the map
+            aktivBoxLayer.addTo(map);
 
 
-            } else {
-                console.warn("Leaflet widget found, but map is not ready yet. Retrying...");
-                setTimeout(() => addMarkerToLeafletMap(latM, lonM, flag, icon1, icon2), 500); // Retry after 500ms
-            }
         } else {
-            console.warn("Leaflet widget not found. Retrying...");
+            console.warn("Leaflet widget found, but map is not ready yet. Retrying...");
             setTimeout(() => addMarkerToLeafletMap(latM, lonM, flag, icon1, icon2), 500); // Retry after 500ms
         }
+    } else {
+        console.warn("Leaflet widget not found. Retrying...");
+        setTimeout(() => addMarkerToLeafletMap(latM, lonM, flag, icon1, icon2), 500); // Retry after 500ms
     }
+}
 
 
-
-})
 
 
 async function fetchrrst(boxids) {
