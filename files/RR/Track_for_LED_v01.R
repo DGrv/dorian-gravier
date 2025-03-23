8ead77743ea09270ac15f990336d4741938b7cfe
DGrv <dorian.gravier@gmail.com>
2025-03-23 14:27:56 +0100

    up

--- files/RR/Track_for_LED_v01.R
+++ files/RR/Track_for_LED_v01.R
@@ -16,9 +16,23 @@ create.dir(wd, "TrackPNG", "wd2")
 
 for(i in 1:length(u(data0$file)) ) {
   
-  a <- ggplot(data0[file == u(file)[i]], aes(lon, lat))+
+  tempTP <- tp[grepl("start|Finish", TimingPoint, ignore.case = T)]
+  debug.easy(nrow(tempTP) > 1, "several finish ? nrow > 1")
+  
+  temp <- data0[file == u(file)[i]]
+  temp[, latSF := tempTP$lat]
+  temp[, lonSF := tempTP$lon]
+  temp[, latdiff := abs(latSF-lat)]
+  temp[, londiff := abs(lonSF-lon)]
+  tempTP[, lat := temp[latdiff == min(latdiff)]$lat]
+  tempTP[, lon := temp[londiff == min(londiff)]$lon]
+  
+  
+  a <- ggplot(temp, aes(lon, lat))+
     geom_path(size=4, color = "#0E3A2F")+
-    geom_point(data = tp[TimingPoint %like%])
+    geom_point(data = tempTP[grepl("start|Finish", TimingPoint, ignore.case = T)], size = 30, color = "#0E3A2F")+
+    geom_point(data = tempTP[grepl("start|Finish", TimingPoint, ignore.case = T)], size = 15, color = "#78FAAE")
+  a
   
   fout <- p0(gsub(".gpx$", ".png", u(data0[file == u(file)[i]]$file)))
   printfast(a, fout, ext = "png", height = 600, width = 600*1.41) # 1.41 is ration lat lon
