d935aac2ce7a60c3d21dd20f548198ec7b5a7e9e
DGrv <dorian.gravier@gmail.com>
2025-11-06 09:59:17 +0100

    up

--- files/Batch/Context_Menu/sesExtractWrapper.bat
+++ files/Batch/Context_Menu/sesExtractWrapper.bat
@@ -3,5 +3,5 @@ REM %~nx1 = filename with extension, no path
 REM optional: cd /d %~dp1  to switch to file directory
 cd /d %~dp1
 REM echo %CD%
-bash -c "source ~/.bashrc;sesExtract '%~nx1'"
+C:\Users\doria\AppData\Local\Microsoft\WindowsApps\bash.exe -c "source ~/.bashrc;sesExtract '%~nx1'"
 REM pause
