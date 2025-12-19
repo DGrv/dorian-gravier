a172dc530909f4a8bc4aa2bd1d36a76e6a2a5066
DGrv <dorian.gravier@gmail.com>
2025-12-19 08:40:51 +0100

    up

--- files/gpx/Run_R_gpx.bat
+++ files/gpx/Run_R_gpx.bat
@@ -2,9 +2,12 @@ REM C:\Users\doria\scoop\apps\r\current\bin\x64\Rscript.exe C:\Users\doria\Downl
 REM C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\R_Create_variable_for_js-html.R
 REM C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Climbing_githubpage.R
 
+
+
+
 C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\Leaflet_github.R
-C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\BikeTrip2022_github.R
+REM C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\BikeTrip2022_github.R
 C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\BikeTrip2025_github.R
 C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\BikeTrip2025_calculation.R
-C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\Climbing_github.R
-C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\PlanMap_github.R
+REM C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\Climbing_github.R
+REM C:\Users\doria\scoop\shims\rscript.exe C:\Users\doria\Downloads\GitHub\dorian.gravier.github.io\files\R\Map_Github\PlanMap_github.R
